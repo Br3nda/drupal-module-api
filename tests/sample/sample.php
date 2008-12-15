56a46e147a66a3b27ba471ba5dac77750444087c
drumm <drumm>
2008-12-15 07:16:19 +0000

    Making a proper database-independent test environment.

---
+++ tests/sample/sample.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * A sample file.
+ */
+
+/**
+ * A sample function.
+ *
+ * Use for sample-related purposes.
+ *
+ * @param $parameter
+ *   A generic parameter.
+ */
+function sample_function($parameter) {
+}
